fc0b22b3602c2acae400785f9a391f4bb740939d
Malathi K <malathi.k@msf.ad>
2022-08-10 18:53:59 +0530

    Added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Signup from './components/Signup/Signup';
+import appStyles from './app.module.css';
+
+function App() {
+  return (
+    <div className={appStyles.contents}>
+        <Signup/>
+    </div>
+  );
+}
+
+export default App;
